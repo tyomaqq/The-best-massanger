77f6e82df80d8a435d4ee411e1b21246cafd5512
Tyoma <tyomaartyomxx@mail.ru>
2022-03-24 19:34:23 +0500

    client.py

--- client.py
+++ client.py
@@ -1,13 +1,14 @@
 
-from http import client
+
+
 import socket
 socket.client = socket.socket(
 socket.AF_INET,
 socket.SOCK_STREAM,
 )
-client.connect(
+socket.client.connect(
      ("127.0.0.1", 1234)
 )
 while True:
-    data = client.recv(2048)
+    data = socket.client.recv(2048)
     print(data.decode("utf-8"))
